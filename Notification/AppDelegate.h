cf825f0fc525f6f851614e78d69711172f4113c3
devalmon <phlood@yandex.ru>
2020-05-21 17:40:57 +0300

    Initial Commit

---
+++ Notification/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Notification
+//
+//  Created by Alexey Baryshnikov on 21.05.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
